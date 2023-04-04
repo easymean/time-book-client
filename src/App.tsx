98eb17bcc6d8b2e2719497e12bcf591b54a10acf
easymean <easymean@ncsoft.com>
2023-04-04 09:42:40 +0900

    모바일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 function App() {
-  return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
-  );
+	return (
+		<>
+			<BrowserView>
+				<div>
+					<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				</div>
+			</BrowserView>
+			<MobileView>
+				<div>
+					<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				</div>
+			</MobileView>
+		</>
+	);
 }
 
 export default App;
