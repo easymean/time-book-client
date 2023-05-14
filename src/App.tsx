3517b59644f2c7907040e42b92a1daa1208173de
easymean <m2162003@naver.com>
2023-05-14 16:23:30 +0900

    react-query 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { BrowserView, MobileView } from 'react-device-detect';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
+import React from 'react';
+
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+	import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(d => ({
+		default: d.ReactQueryDevtools,
+	})),
+);
 
 function App() {
+	const [showDevtools, setShowDevtools] = React.useState(false);
+
+	React.useEffect(() => {
+		// @ts-ignore
+		window.toggleDevtools = () => setShowDevtools(old => !old);
+	}, []);
+
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<BrowserView>
 				<div>
 					<h1 className="text-3xl font-bold underline">Hello world!</h1>
@@ -14,7 +32,13 @@ function App() {
 					<h1 className="text-3xl font-bold underline">Hello world!</h1>
 				</div>
 			</MobileView>
-		</>
+			<ReactQueryDevtools initialIsOpen />
+			{showDevtools && (
+				<React.Suspense fallback={null}>
+					<ReactQueryDevtoolsProduction />
+				</React.Suspense>
+			)}
+		</QueryClientProvider>
 	);
 }
 
