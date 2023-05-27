396707c5d781068f7e364f72fea8ea86bb7fc49b
easymean <m2162003@naver.com>
2023-05-28 04:03:39 +0900

    vite 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,14 @@ export default defineConfig({
 	// scss 전역 사용
 	css: {
 		preprocessorOptions: {
-			scss: { additionalData: '@import "./src/css/colors";' },
+			scss: { additionalData: '@import "./src/styles/colors";' },
+		},
+	},
+	build: {
+		emptyOutDir: true,
+		lib: {
+			name: 'TimeBook',
+			entry: path.resolve(__dirname, 'src/main.tsx'),
 		},
 	},
 });
