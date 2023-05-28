9306440cf01aed82737e8c00d6d7372d09632307
easymean <m2162003@naver.com>
2023-05-28 15:41:15 +0900

    css-module 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,34 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
+type ViteConfig = {
+	mode: string;
+	command: string;
+};
 
-	// scss 전역 사용
-	css: {
-		preprocessorOptions: {
-			scss: { additionalData: '@import "./src/styles/colors";' },
+// https://vitejs.dev/config/
+export default (args: ViteConfig) => {
+	const generateScopedName =
+		args.mode === 'production' ? '[hash:base64:2]' : '[name]_[local]__[hash:base64:2]';
+	return defineConfig({
+		plugins: [react()],
+
+		// scss 전역 사용
+		css: {
+			modules: {
+				localsConvention: 'camelCase',
+				generateScopedName,
+			},
+			preprocessorOptions: {
+				scss: { additionalData: '@import "./src/styles/colors";' },
+			},
 		},
-	},
-	build: {
-		emptyOutDir: true,
-		lib: {
-			name: 'TimeBook',
-			entry: path.resolve(__dirname, 'src/main.tsx'),
+		build: {
+			emptyOutDir: true,
+			lib: {
+				name: 'TimeBook',
+				entry: path.resolve(__dirname, 'src/main.tsx'),
+			},
 		},
-	},
-});
+	});
+};
